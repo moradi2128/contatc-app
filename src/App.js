2f400c3cec93d4ddfc084c36f1cafdcf1b52da28
mohammadreza moradi <moradi2565@gmail.com>
2022-02-10 21:43:15 +0330

    install redux bootstrap router toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { ToastContainer } from "react-toastify";
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
