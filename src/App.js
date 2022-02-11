132a676d08737346e95f02b5a1d90e92ac9e3ed6
mohammadreza moradi <moradi2565@gmail.com>
2022-02-11 17:43:44 +0330

    finish section one(style base,redux)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import NavBar from "./components/NavBar";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import Home from "./components/Home";
+import NotFound from "./components/404";
 
 function App() {
   return (
-    <div className="App">
-
-      <ToastContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <ToastContainer />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/edit/:editId" element={<Edit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
